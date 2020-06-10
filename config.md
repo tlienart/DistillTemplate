db7cd5032681f3526877edfa9eaa73de74048fbf
Jun Tian <tianjun.cpp@gmail.com>
2020-06-10 13:52:50 +0800

    set aside as lx command

--- config.md
+++ config.md
@@ -5,5 +5,3 @@
 
 \newcommand{\R}{\mathbb R}
 \newcommand{\scal}[1]{\langle #1 \rangle}
-
-\newcommand{\aside}[1]{<aside> #1 </aside>}
--- utils.jl
+++ utils.jl
@@ -1,3 +1,6 @@
+using Franklin
+const F = Franklin
+
 # TODO: support YAML format
 function hfun_frontmatter()
     fm = locvar(:front_matter)
@@ -12,4 +15,9 @@ function hfun_frontmatter()
         </d-front-matter>
         """
     end
+end
+
+function lx_aside(lxc,_)
+    content = F.md2html(F.content(lxc.braces[1]))
+    "<aside>$content</aside>"
 end
